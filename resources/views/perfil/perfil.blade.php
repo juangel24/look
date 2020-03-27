876e43a02ca188be2415509ed00a9174c15ff7de
iovanna escarcega <iovassoto@gmail.com>
2020-03-27 14:19:18 -0600

    perfil

--- resources/views/perfil/perfil.blade.php
+++ resources/views/perfil/perfil.blade.php
@@ -1,14 +1,16 @@
-@extends('base.html')
+@extends('layout.base')
 
 @section('content')
 <div class="container perfil">
     <div class="row">
       <div class="col-md-6 perfilfoto" id="perfilfoto">
         <div class="media" id="divmedia">
-            <img class="d-flex mr-3" id="fotodeperfil" src="https://mdbootstrap.com/img/Photos/Others/placeholder1.jpg" alt="Generic placeholder image">
+            <a href="" type="button"><img class="d-flex mr-3" id="fotodeperfil" src="https://mdbootstrap.com/img/Photos/Others/placeholder1.jpg" alt="Generic placeholder image"></a>
             <div class="media-body " id="mediaperfil">
-              <h5 class="mt-0 mb-2 font-weight-bold">Usuario</h5></a>
-              
+              <h5 class="mt-0 mb-2 font-weight-bold">Usuario&nbsp;&nbsp;
+                <a href="" type="button" class="iconsperfil"><i class="fas fa-user-edit"></i></a>
+                &nbsp;&nbsp;<a class="iconsperfil" href="" type="button"><i class="fas fa-cog"></i></a>
+              </h5></a>
              <br>
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
               vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia
@@ -18,7 +20,7 @@
                 
             </div>
           </div>
-          <a href="" type="button" style="width:10%;height:10%"><i class="fas fa-user-edit"></i></a>
+         
       </div>
       
       <div class="col-md-6 descripcion">
