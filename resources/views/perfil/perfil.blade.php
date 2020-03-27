8a1d08ab87f2b0cd91c90b9ec3f2b259a7531d55
iovanna escarcega <iovassoto@gmail.com>
2020-03-27 15:31:04 -0600

    modal en foto de perfil

--- resources/views/perfil/perfil.blade.php
+++ resources/views/perfil/perfil.blade.php
@@ -8,12 +8,39 @@
     <div class="row">
       <div class="col-md-6 perfilfoto" id="perfilfoto">
         <div class="media" id="divmedia">
-            <a href="" type="button"><img class="d-flex mr-3" id="fotodeperfil" src="https://mdbootstrap.com/img/Photos/Others/placeholder1.jpg" alt="Generic placeholder image"></a>
+            <a  type="button" data-toggle="modal" data-target="#exampleModalCenter">
+              <img class="d-flex mr-3" id="fotodeperfil" src="https://mdbootstrap.com/img/Photos/Others/placeholder1.jpg" alt="Generic placeholder image">
+
+   <!-- Modal -->
+      <div class="modal fade " id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle"aria-hidden="true">
+  <!-- Add .modal-dialog-centered to .modal-dialog to vertically center the modal -->
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content modalperfil1">
+        <div class="modal-header modalperfil">
+         <h5 class="modal-title" id="exampleModalLongTitle"><strong>Cambiar foto de perfil</strong></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+    <div class="modal-body">
+      <div class="subirfoto">
+        <h6>Subir foto</h6>
+      </div>
+      <hr>
+      <div class="eliminarfoto">
+        <h6>Eliminar foto actual</h6>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+            <!-- Central Modal Small -->
+            </a>
             <div class="media-body " id="mediaperfil">
               <h5 class="mt-0 mb-2 font-weight-bold">Usuario&nbsp;&nbsp;
-                <a href="" type="button" class="iconsperfil"><i class="fas fa-user-edit"></i></a>
-                &nbsp;&nbsp;<a class="iconsperfil" href="" type="button"><i class="fas fa-cog"></i></a>
-              </h5></a>
+                  <a href="" type="button" class="iconsperfil"><i class="fas fa-user-edit"></i></a>
+                  &nbsp;&nbsp;<a class="iconsperfil" href="" type="button"><i class="fas fa-cog"></i></a>
+                 </h5></a>
              <br>
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
               vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia
@@ -38,5 +65,9 @@
      
     </div>
     <hr>
-  </div>
+
+</div>
+@endsection
+@section('javascript')
+    <script src="js/perfil.js"></script>
 @endsection
