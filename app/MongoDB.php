12886ccb314ddc58ada69c7de28f6464125f1f30
Juan Angel <Juangel2406@gmail.com>
2020-03-31 20:38:34 -0600

    Mongo object restored

---
+++ app/MongoDB.php
@@ -0,0 +1,19 @@
+<?php
+namespace App;
+
+use MongoDB\Client as Mongo;
+
+class MongoDB {
+    private $mongo;
+
+    function __construct() {
+        $this->mongo = new Mongo("mongodb://localhost:27017");
+    }
+
+    function test() {
+        $collection = $this->mongo->chat->persons;
+        $result = $collection->find()->toArray();
+
+        dd($result);
+    }
+}
